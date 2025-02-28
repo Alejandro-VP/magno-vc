e25da14e1ee06f16a56012cc94cd5a9aba664d92
Alejandro-VP <alejandrovp1733@gmail.com>
2025-02-28 13:47:04 +0100

    añadida la configuración de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    // Tu configuración de Firebase
+    apiKey: "AIzaSyDHUqjOP5338am_v5ae7CFW6Y-pvDzNOAo",
+    authDomain: "magno-vc.firebaseapp.com",
+    projectId: "magno-vc",
+    storageBucket: "magno-vc.firebasestorage.app",
+    messagingSenderId: "483095242852",
+    appId: "1:483095242852:web:59b5dfeee64adb9a9f9771",
+    measurementId: "G-SPXENWW447"
 };
 
 const app = initializeApp(firebaseConfig);
